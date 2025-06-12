9c6628edfb33daee2227e4c5981de16b872423a1
nkuue <nkuuee@bk.ru>
2025-06-12 16:21:42 +0300

    feat: класс GameStatus отвечает за текущее состояние игры

---
+++ src/main/java/game/GameStatus.java
@@ -0,0 +1,18 @@
+package game;
+
+public class GameStatus {
+    private static final int MAX_MISTAKES = 10;
+    private int currentMistakes = 0;
+
+    public void incrementMistakes() {
+        currentMistakes++;
+    }
+
+    public int getCurrentMistakes() {
+        return currentMistakes;
+    }
+
+    public boolean isGameOver() {
+        return currentMistakes >= MAX_MISTAKES;
+    }
+}
